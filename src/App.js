95b21c851d4cbce1b3d28ab6fbecaf8c2bcd5ec9
june <juneandkang@gmail.com>
2023-02-16 11:08:17 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DiaryEditor from './DiaryEditior';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <DiaryEditor />
+    </div>
+  );
+}
+
+export default App;
